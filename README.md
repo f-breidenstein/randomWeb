a8ef6ad643ca623aea7db5e623e66ce2a01d476d
Felix Breidenstein <mail@felixbreidenstein.de>
2014-03-20 19:22:40 +0100

    First version. Needs more errorhandling to work good

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ randomWeb
 =========
 
 Expore the interwebz with a bit of randomness
+
+Requirements:
+- Python2
+- BeautifulSoup
+- urllib2
---
+++ web.py
@@ -0,0 +1,57 @@
+#! /usr/bin/enx python2
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import random
+from urlparse import urlparse
+
+
+def getNextUrl(url):
+    """ Return one random url from a given website """
+    result = []
+    try:
+        response = urllib2.urlopen(url)
+        the_page = response.read()
+        pool = BeautifulSoup(the_page)
+        result = pool.findAll('a')
+    except:
+        # Errors should be catched here
+        pass
+
+
+    if len(result) == 0:
+        # Return the old url if nothing was found
+        return url
+    else:
+        num = random.randrange(len(result))
+        newUrl = result[num]['href']
+
+        if newUrl[0] == '/':
+            # Add root domain at the begin if it is a relative link
+            return "http://" + urlparse(url)[1] + newUrl
+
+        if newUrl[0] == '.':
+            return "http://" + urlparse(url)[1] + newUrl[1:]
+
+        if newUrl[0] == '#':
+            return url
+
+        else:
+            return newUrl
+
+
+def main():
+    steps = 10
+    verbose = True
+    url = "http://www.spiegel.de"
+
+    print "Start: " + url
+
+    for i in range(steps):
+        if (verbose):
+            print str(i) + ": " + url
+        url = getNextUrl(url)
+
+    print "End: " + url
+
+if __name__ == "__main__":
+    main()
